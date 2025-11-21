7833b0923c97bfc0dc243d8402bb3c50b75596a1
bryanwessantana <bryanwessantana@gmail.com>
2025-11-21 18:02:37 -0300

    Fixing the nav bar to have the "New Car" option

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -2,60 +2,69 @@ const utilities = require('../utilities');
 const accountModel = require('../models/account-model');
 const bcrypt = require('bcryptjs');
 
-// Deliver login view
 const buildLogin = utilities.handleErrors(async function (req, res, next) {
     let nav = await utilities.getNav()
     res.render('account/login', {
         title: 'Login',
         nav,
         errors: null,
+        account_email: null, 
     });
 });
 
-// Deliver registration view
 const buildRegister = utilities.handleErrors(async function (req, res, next) {
     let nav = await utilities.getNav()
     res.render('account/register', {
         title: 'Register',
         nav,
         errors: null,
+        account_firstname: null,
+        account_lastname: null,
+        account_email: null,
     });
 });
 
-// Process registration
 const registerAccount = utilities.handleErrors(async function (req, res, next) {
     let nav = await utilities.getNav()
-    const { firstName, lastName, email, password } = req.body;
+    const { 
+        account_firstname, 
+        account_lastname, 
+        account_email, 
+        account_password 
+    } = req.body;
 
-    // Hash the password before storing
     let hashedPassword;
     try {
-        hashedPassword = await bcrypt.hash(password, 10);
+        hashedPassword = await bcrypt.hash(account_password, 10);
     } catch (error) {
         req.flash("notice", "Error processing registration.");
         res.status(500).render('account/register', {
             title: 'Register',
             nav: nav,
             errors: null,
+            account_firstname,
+            account_lastname,
+            account_email,
         });
         return;
     }
 
     const regResult = await accountModel.registerAccount(
-        firstName,
-        lastName,
-        email,
+        account_firstname,
+        account_lastname,
+        account_email,
         hashedPassword
     );
 
     if (regResult) {
         req.flash(
-            "notice", `Congratulations, you're registered ${firstName}. Please log in.` // FIXED template literal
+            "notice", `Congratulations, you're registered ${account_firstname}. Please log in.`
         )
         res.status(201).render('account/login', {
             title: 'Login',
             nav,
             errors: null,
+            account_email: account_email,
         });
     } else {
         req.flash("notice", "Sorry, the registration failed.");
@@ -63,9 +72,9 @@ const registerAccount = utilities.handleErrors(async function (req, res, next) {
             title: 'Register',
             nav,
             errors: null,
-            firstName,
-            lastName,
-            email,
+            account_firstname,
+            account_lastname,
+            account_email,
         });
     }
 });
