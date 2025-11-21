b608c76a47502b07347de31cac2d50bcab041bec
bryanwessantana <bryanwessantana@gmail.com>
2025-11-21 18:04:28 -0300

    Adding the DOM Content on script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,20 @@
-const passBtn = document.querySelector("#passBtn");
-passBtn.addEventListener("click", function (event) {
-
-    event.preventDefault(); 
+document.addEventListener("DOMContentLoaded", function () {
+    const passBtn = document.querySelector("#passBtn");
     
-    const passInput = document.getElementById("account_password");
-    const type = passInput.getAttribute("type");
-    
-    if (type === "password") {
-        passInput.setAttribute("type", "text");
-        passBtn.innerHTML = "Hide Password";
-    } else {
-        passInput.setAttribute("type", "password");
-        passBtn.innerHTML = "Show Password";
+    if (passBtn) {
+        passBtn.addEventListener("click", function (event) {
+            event.preventDefault(); 
+            
+            const passInput = document.getElementById("account_password");
+            const type = passInput.getAttribute("type");
+            
+            if (type === "password") {
+                passInput.setAttribute("type", "text");
+                passBtn.innerHTML = "Hide Password";
+            } else {
+                passInput.setAttribute("type", "password");
+                passBtn.innerHTML = "Show Password";
+            }
+        });
     }
 });
